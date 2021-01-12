981470cdb1ce1b53bf9155fa7710b5bd6d371ddf
Jeroen van Maanen <jeroen@leialearns.org>
2021-01-12 09:29:31 +0100

    Renamed package to "dendrite". Added package data for "crates.io".

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,16 @@ use log::info;
 
 use tonic::transport::Server;
 
-use rustic_dendrite::example_api::init;
-use rustic_dendrite::example_command::handle_commands;
-use rustic_dendrite::example_event::process_events;
-use rustic_dendrite::example_query::process_queries;
-use rustic_dendrite::grpc_example::greeter_service_server::GreeterServiceServer;
+use dendrite::example_api::init;
+use dendrite::example_command::handle_commands;
+use dendrite::example_event::process_events;
+use dendrite::example_query::process_queries;
+use dendrite::grpc_example::greeter_service_server::GreeterServiceServer;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
-    info!("Rustic dendrite API service started");
+    info!("Dendrite API service started");
 
     let greeter_server = init().await.unwrap();
 
